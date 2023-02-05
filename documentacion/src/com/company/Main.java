b7b62b6dafaa8c6f6537ede0dd9f8d115d9bb9d1
Milton Figueredo Miles <gratno@gmail.com>
2023-02-05 18:07:15 -0500

    Comentarios en Java /**

--- documentacion/src/com/company/Main.java
+++ documentacion/src/com/company/Main.java
@@ -1,10 +1,53 @@
 package com.company;
 
+import javax.swing.*;
+
+/** SÓLO COMENTARIOS QUE APORTEN VALOR
+ * POCOS Y MUY CONCRETOS
+ *
+ * CLASE PRINCIPAL
+ * @AUTOR   ARMANDO PROGRAMAS
+ * @version 1-0
+ * @since 2023
+ */
+
 public class Main {
+
+    //COMENTARIOS DE MANTENIMIENTO DE CÓDIGO
+    //CON 2 ASTERISCOS /**
+
+    /**
+     *FUNCION PRINCIPAL
+     *
+     * @param args
+     *
+     */
+
 
     public static void main(String[] args) {
         //Comentarios en JAVA
+        //write your code here
 
-        String string
+        /*Aqui va texto
+
+        SE PUEDE AGRGAR UN INDICE
+        DE TAREAS O PASOS A SEGUIR
+
+        1. ABRIRI FICHERO
+        2. REVISAR ANOTACIONES
+        4. PROCESAR FICHERO
+        5. VER BUGS
+        6. CORRECIÓN Y REFACTORIZAR
+        7. CERRAR FICHERO
+
+        ENTRE MAS COMPLEJO EL CÓDIGO MAS
+        INFORMACIÓN Y COMENTARIOS DEBEMOS INCLUIR
+         */
+
+        //Imprimir texto
+        System.out.println("Activividaes por hacer"); //Imprimir texto
+
+
+
     }
 }
