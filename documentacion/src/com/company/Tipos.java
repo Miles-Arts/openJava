73881c9876ffe712619af35f8d279b6c55a391e4
Milton Figueredo Miles <gratno@gmail.com>
2023-02-05 18:49:57 -0500

    Tipos de datos

---
+++ documentacion/src/com/company/Tipos.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        //VARIABLES QUE ESPECIFICA TIPOS DE DATOS
+
+        //                       valor int o STRING
+        //Tipo indentificador = 30;  INT
+        //Tipo indentificador = "JAVA"; STRING
+        //identificador = 30
+
+
+        //Enteros PRIMITIVOS
+
+        //para numeros pequeños
+        byte number1 = 1; // 1 byte
+        short number2 = 2; // 2 byte
+
+        //Para numeros grandes son los más usados
+        int number3 = 3; // 4 bye
+        long number4 = 4; // 8 byte
+
+
+        //Punto Flotante
+
+        // se puede añadir f para indicar que es float 4.9f;
+        float decimal = (float) 4.9;
+        float decimall = 4.9f;
+
+        //se puede agrgar la d para indicar que es double
+        double decimal2 = 9.99d; // numeros float grandes
+
+
+        //caracter
+        // para una letra
+        char chacarter = "a";
+
+
+        //booleanos VERDADEROS o FALSOS
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //Cadenas de texto
+        String nombre = "Java";
+        String apellido = "Aprendo";
+
+        //tipos envoltorio o WRAPPERs
+
+        Integer numero = null;
+        Integer numeroo = 1330;
+
+        //se puede añadir la L para indicar Long
+        Long  numero2 = 21L;
+        Long  numerro = 21;
+
+
+    }
+}
