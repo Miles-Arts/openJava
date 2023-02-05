2da58d814c1164f6fef4553dc63cecaf7bfabd29
Milton Figueredo Miles <gratno@gmail.com>
2023-02-05 17:44:54 -0500

    Empezando nuevo proyecto Documentación JAVA

---
+++ src/com/company/ByeWorld.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ByeWorld {
+
+    public static void main(String[] args) {
+
+        String string = "Bye hasta en unos minutos";
+
+        System.out.println(string);
+    }
+}
--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -5,9 +5,10 @@ public class HelloWorld {
 
 
     // metodo de entrada a una aplicación JAVA
+    //MAIN metodo principal
     public static void main(String[] args) {
 
-       String string = "Hello Word";
+       String string = "Hello Word mejorando mis conocimientos en Java";
 
         System.out.println(string);
 
