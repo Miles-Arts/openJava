a1ce09fbbfebf3aa9a8b618d9887a3b608040030
Milton Figueredo Miles <gratno@gmail.com>
2023-02-05 16:51:09 -0500

    hola

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.company;
+
+//Clase de Java
+public class HelloWorld {
+
+
+    // metodo
+    public static void main(String[] args) {
+
+       String string = "Hello Word";
+
+        System.out.println(string);
+
+
+    }
+
+}
