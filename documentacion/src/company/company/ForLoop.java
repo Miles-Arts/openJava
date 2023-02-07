7e486d9cd39865d10e48316a6c9809e90003cfd3
Milton Figueredo Miles <gratno@gmail.com>
2023-02-07 13:32:14 -0500

    Ciclos

---
+++ documentacion/src/company/company/ForLoop.java
@@ -0,0 +1,15 @@
+package company.company;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        // los for sirven con Arrays
+        //   comienzo | Parada | Paso
+         for (int i = 0; i <= 10; i++ ) { //Estructura de control determinada
+             System.out.println("Print: " + i + " Hi Startups");
+         }
+        System.out.println("for terminado");
+    }
+
+}
---
+++ documentacion/src/company/company/WhileLoop.java
@@ -0,0 +1,19 @@
+package company.company;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        //condición
+        //boolean check = true;
+   //se usan los contadores
+        int count = 0;
+
+        //En los parentesis se evalua una codición
+        while (count < 10 )  {
+            System.out.println(count + " Dedicación y Esfuerzo");
+            count++; //condición
+        }
+        System.out.println("Fin");
+    }
+}
