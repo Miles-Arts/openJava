3860a2cd6fd5d9e304c414f6a45a3fea1539e776
Milton Figueredo Miles <gratno@gmail.com>
2023-02-06 19:56:52 -0500

    Revisando funciones

--- documentacion/src/com/company/Funciones.java
+++ documentacion/src/com/company/Funciones.java
@@ -1,28 +1,46 @@
 package com.company;
 
+
 public class Funciones {
 
-    public static void main(String[] args) {
+    public static <Int> void main(String[] args) {
 
-        System.out.println("Hola mundo sin método");
+        //System.out.println("Hola mundo sin método");
 
         //Una función se escribe y tiene parentesis
 
         //holaMundo();
         //holaMundo();
-        holaMundo();
+        //devolverHolaMundo("Java es fácil");
+
+        int edad = edadHumano();
+
+        String hola = devolverHolaMundo();
+        System.out.println(hola);
         holaMundo("Saludos");
+        System.out.println(edad);
     }
     //Hola mundo es el Indentifacor
     private static void holaMundo() {
 
-        System.out.println("Hola mundo desde un método");
+        //System.out.println("Hola mundo desde un método");
 
     }
 
-    private static void holeMundo(String name) {
+    public static void holaMundo(String name) {
 
-        System.out.println("Hola " + name);
+        System.out.println("Lunes de " + name);
 
+    }
+
+    private static String devolverHolaMundo() {
+
+        return "Devolver Texto";
+
+    }
+
+    private static <Int> int edadHumano() {
+
+        return 38;
     }
 }
