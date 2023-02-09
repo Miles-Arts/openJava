6c3b11490f89a0729eb1b2ba39a7217aa39a8e39
Milton Figueredo Miles <gratno@gmail.com>
2023-02-09 18:52:52 -0500

    Polimorfimo

---
+++ documentacion/src/company/company/PolimorfismoMain.java
@@ -0,0 +1,36 @@
+package company.company;
+
+public class PolimorfismoMain {
+
+    public static void main(String[] args) {
+
+
+        Coche coche1 = new Coche();
+
+        CocheElectrico coche2 = new CocheElectrico();
+
+        CocheHibrido coche3 = new CocheHibrido();
+
+        //POLIMORFISMO
+    //tipo    base        tipo hijo
+        Coche coche4 = new CocheElectrico();
+
+        Coche coche5 = new CocheHibrido();
+
+
+        //INSTANCE comprueba la instacia de la clase,
+        //ver de cuál es hijo
+        if (coche4 instanceof CocheElectrico) {
+            System.out.println("coche eléctico");
+        }
+
+        if (coche4 instanceof Coche) {
+            System.out.println("coche C");
+        }
+
+        if (coche4 instanceof CocheHibrido) {
+            System.out.println("coche hibrido");
+        }
+
+    }
+}
