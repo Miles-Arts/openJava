6aa5bc8afa76f4c057188637fd6efc003da8d466
Milton Figueredo Miles <gratno@gmail.com>
2023-02-10 21:49:12 -0500

    Método de String

---
+++ documentacion/src/company/company/StringMain.java
@@ -0,0 +1,23 @@
+package company.company;
+
+public class StringMain {
+
+    public static void main(String[] args) {
+
+
+        //La clase String
+
+        /*
+
+        length()
+        startsWith("")
+        endsWitch("")
+        IndexOf(1,5)
+        trim()
+        equals()
+        compareTo
+
+         */
+
+    }
+}
