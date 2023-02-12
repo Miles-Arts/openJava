b122fda7ce44fddd1809ef76ad501571bb5aa226
Milton Figueredo Miles <gratno@gmail.com>
2023-02-11 22:49:47 -0500

    Map Java

---
+++ documentacion/src/company/company/MapMain.java
@@ -0,0 +1,35 @@
+package company.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain {
+
+    public static void main(String[] args) {
+
+
+        //Clave   -  Valores
+        Map<String, String> personas = new HashMap<>();
+
+
+        personas.put("A-82828" , "Nombre Apellido 1");
+        personas.put("B-82828" , "Nombre Apellido 2");
+        personas.put("C-82828" , "Nombre Apellido 3");
+
+        System.out.println(personas);
+
+        for (String value : personas.keySet()) {
+            System.out.println(value);
+        }
+
+        for (String key : personas.values()) {
+            System.out.println(key);
+        }
+
+        for (Map.Entry<String , String> pair : personas.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+
+    }
+
+}
